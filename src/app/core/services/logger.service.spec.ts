bb9b5071f3ffb1f012725ed7ec47d11b403d8065
Pavitrakumari <pavithra.korapati@gmail.com>
2018-11-08 14:55:48 +0530

    08/11/2018

---
+++ src/app/core/services/logger.service.spec.ts
@@ -0,0 +1,18 @@
+/**Configures and initializes environment for unit testing and provides methods for 
+ * creating components and services in unit tests.*/
+import { TestBed } from '@angular/core/testing';
+import { LoggerService } from './logger.service';
+describe('LoggerService', () => {
+  let loggerService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [LoggerService]
+    });
+    loggerService = TestBed.get(LoggerService);
+  });
+  it('should log without errors', (() => {
+    expect(loggerService).toBeDefined();
+    expect(LoggerService.error('This is an error')).toBeUndefined();
+    expect(LoggerService.log('This is a log')).toBeUndefined();
+  }));
+});
---
+++ src/app/core/services/logger.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+@Injectable({ providedIn: 'root' })
+export class LoggerService {
+  static log(msg: string, obj = {}): void {
+    console.log(msg, obj);
+  }
+  static error(msg: string, obj = {}): void {
+    console.error(msg, obj);
+  }
+}
+
+
