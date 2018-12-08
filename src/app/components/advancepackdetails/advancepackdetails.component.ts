a5e444832c514ec319a48ebfebfb43a5d5ddba1e
Pavitrakumari <pavithra.korapati@gmail.com>
2018-12-08 18:38:49 +0530

    PPP

--- src/app/components/advancepackdetails/advancepackdetails.component.ts
+++ src/app/components/advancepackdetails/advancepackdetails.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { UserService } from '../../core/services/http/user/user.service';
 import { Notes } from '../../core/models/notes';
+import { CartService } from '../../core/services/http/cart/cart.service';
 
 @Component({
   selector: 'app-advancepackdetails',
@@ -13,11 +14,16 @@ export class AdvancepackdetailsComponent implements OnInit {
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   list:Notes[]=[]
+  private price: any;
+  private desc: any;
+ private name: any;
+  productId1: any;
 
-  constructor(private _formBuilder: FormBuilder,private userService:UserService) { }
+  constructor(private _formBuilder: FormBuilder,private cartService:CartService) { }
   private cards=[];
 
   ngOnInit() {
+    this.getCartDetails();
     this.firstFormGroup = this._formBuilder.group({
       firstCtrl: ['', Validators.required]
     });
@@ -27,19 +33,22 @@ export class AdvancepackdetailsComponent implements OnInit {
 
 
 
-    let obs = this.userService.getDataService1();
-    obs
-    // .pipe(takeUntil(this.destroy$))
-    .subscribe((response) => {
-      console.log("success in cart getcards()",response);
-      this.list = response["data"].data;
-      for (let i = 0; i <this.list .length; i++) {
-        this.cards.push(this.list [i]);
-      }
-    })
+
   
   }
-
+  private product=localStorage.getItem('productId')
+  getCartDetails(){
+    this.cartService.cartDetails(this.product).subscribe(response=>{
+    console.log('cartDetails',response);
+    this.productId1=response['data']['product']['id']
+    console.log("productid",this.productId1);
+    this.price=response['data']['product']['price']
+    this.desc=response['data']['product']['description']
+    this.name=response['data']['product']['name']
+  });
+    
+    }
+  
   
 
 }
